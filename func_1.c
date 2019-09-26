44cfdb6b48662119e825cdb03bbb7b6ce2d37907
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-09-26 04:24:01 +0000

    [Feature] first functions push and pall

---
+++ func_1.c
@@ -0,0 +1,80 @@
+#include "holberton.h"
+/**
+ *push_stack - adds a node to the stack
+ *@head: head of the stack
+ *@num: number to add to the node
+ */
+void push_stack(stack_t **head, int num)
+{
+	stack_t *new = NULL;
+
+	new = malloc(sizeof(stack_t));
+	if (!new)
+	{
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new->n = num;
+	new->next = *head;
+	new->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = new;
+	*head = new;
+}
+/**
+ *push_queue - adds a node to the queue
+ *@head: front of the queue
+ *@num: number to add to the node
+ */
+void push_queue(stack_t **head, int num)
+{
+	stack_t *new = NULL, *aux = NULL;
+
+	new = malloc(sizeof(stack_t));
+
+	aux = *head;
+	while (aux)
+	{
+		if (aux->next != NULL)
+			aux = aux->next;
+	}
+	new->n = num;
+	new->prev = aux;
+	new->next = NULL;
+	if (aux != NULL)
+		aux->next = new;
+}
+/**
+ *mop_pall - prints number of stack nodes
+ *@head: head of the stack
+ *@line_number: number where command is for possible error mesasge
+ */
+void mop_pall(stack_t **head, unsigned int line_number)
+{
+	stack_t *aux = NULL;
+
+	(void) line_number;
+	aux = *head;
+	while (aux)
+	{
+		printf("%d\n", aux->n);
+		aux = aux->next;
+	}
+}
+/**
+ *mop_pall - prints number of stack nodes
+ *@head: head of the stack
+ *@line_number: number where command is for possible error mesasge
+ */
+void freezer(stack_t **head)
+{
+	stack_t *aux = NULL;
+
+	aux = *head;
+	while (aux)
+	{
+		aux = aux->next;
+		free(*head);
+		*head = aux;
+	}
+}
---
+++ main.c
@@ -0,0 +1,129 @@
+#include "holberton.h"
+/**
+ *main - This is the main function to read monty opcode
+ *@argc: number of arguments (File) to read
+ *@argv: vector containing the arguments
+ *Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	FILE *file_;
+	size_t sizze;
+	stack_t *head = NULL;
+	char *m_code = NULL;
+	unsigned int line_number = 0;
+
+	file_ = fopen(argv[1], "r");
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (!(file_) || file_ == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while ((getline(&m_code, &sizze, file_)) != EOF)
+	{
+		line_number++;
+		tokenizer(m_code, line_number, &head);
+	}
+	free(m_code);
+	freezer(&head);
+	fclose(file_);
+	return (0);
+}
+/**
+ *tokenizer - get tokens from file line
+ *@m_code: line of the document to get token from
+ *@line_number: number of the line where the token belongs
+ *@head: head of the stack
+ *
+ */
+void tokenizer(char *m_code, unsigned int line_number, stack_t **head)
+{
+	char *mc_token = NULL;
+	int status = 0;
+	int n_num = 0;
+
+	mc_token = strtok(m_code, TOKDEL);
+	if (strcmp(mc_token, QUEUE) == 0)
+	{
+		status = 1;
+		mc_token = strtok(NULL, TOKDEL);
+	}
+	else if (strcmp(mc_token, STACK) == 0)
+	{
+		status = 0;
+		mc_token = strtok(NULL, TOKDEL);
+	}
+	while (mc_token)
+	{
+		if (strcmp(mc_token, "push") == 0)
+		{
+			mc_token = strtok(NULL, TOKDEL);
+			if (check_integer(mc_token, line_number))
+			{
+				n_num = atoi(mc_token);
+				if (status == 0)
+					push_stack(head, n_num);
+				else if (status == 1)
+					push_queue(head, n_num);
+			}
+			mc_token = strtok(NULL, TOKDEL);
+		}
+		else
+			s_func(mc_token, line_number, head);
+			mc_token = strtok(NULL, TOKDEL);
+	}
+}
+/**
+ *check_integer - checks if arg for push is an integer
+ *@m_code: string token to be verfifed
+ *@l_n: number of the commmand for possible error
+ *Return: 1 if str is number 0 if not
+ *
+ */
+int check_integer(char *m_code, unsigned int l_n)
+{
+	int count = 0;
+
+	if (m_code[count] == '-')
+		count++;
+	while (m_code[count] != '\0')
+	{
+		if (isdigit(m_code[count]) == 0)
+		{
+			fprintf(stderr, "L%u: usage: push integer", l_n);
+			exit(EXIT_FAILURE);
+		}
+		count++;
+	}
+	return (1);
+}
+/**
+ *s_func - searches for function related to the token
+ *@mc_token: function command tokenized
+ *@l_n: line where command is for possible error message
+ *@head: head of the stack
+ */
+void s_func(char *mc_token, unsigned int l_n, stack_t **head)
+{
+	int count = 0;
+	instruction_t m_opcodes[] = { {"pall", mop_pall}, {NULL, NULL} };
+
+
+	while (m_opcodes[count].opcode != NULL)
+	{
+		if (strcmp(m_opcodes[count].opcode, mc_token) == 0)
+		{
+			m_opcodes[count].f(head, l_n);
+			return;
+		}
+		count++;
+	}
+	printf("L%d: unknown instruction %s\n", l_n, mc_token);
+	exit(EXIT_FAILURE);
+}
