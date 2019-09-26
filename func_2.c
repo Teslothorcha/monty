b7176eb70d147f6280b31c5136423f6c01941db0
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-09-26 22:48:23 +0000

    [Fix] add nwe line to error message

--- func_2.c
+++ func_2.c
@@ -11,7 +11,7 @@ void mop_pop(stack_t **head, unsigned int line_number)
 
 	if (*head == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	aux = *head;
