7ca5cf183d18b37bdadc8c1aef092c0e269dc4c7
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-09-27 04:24:52 +0000

    [Fix] change conditional order

--- func_3.c
+++ func_3.c
@@ -130,7 +130,7 @@ void mop_pstr(stack_t **head, unsigned int l_n)
 
 	(void) l_n;
 	aux = *head;
-	while (aux->n > 0 && aux->n < 128 && aux)
+	while (aux && aux->n > 0 && aux->n < 128)
 	{
 		printf("%c", aux->n);
 		aux = aux->next;
