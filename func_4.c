24c661af27528a76b6197aa63320c8f19054fc93
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-09-27 04:58:00 +0000

    [Feature] add function rotl .c

---
+++ func_4.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+char *mc_token;
+/**
+ *mop_rotl - send top of the stack to the end
+ *@head: head of the stack
+ *@l_n: number where command is for possible error mesasge
+ */
+void mop_rotl(stack_t **head, unsigned int l_n)
+{
+	stack_t *aux = NULL, *aux1 = NULL;
+
+	(void) l_n;
+	aux = *head;
+	aux1 = (*head)->next;
+	if (!(*head) || !(*head)->next)
+	{
+		return;
+	}
+	while (aux->next)
+	{
+		aux = aux->next;
+	}
+	aux->next = *head;
+	(*head)->next = NULL;
+	(*head)->prev = aux;
+	*head = aux1;
+	(*head)->prev = NULL;
+}
